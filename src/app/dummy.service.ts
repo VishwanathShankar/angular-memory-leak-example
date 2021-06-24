2c4170a7fa7883cd5667fb60ed8c7abdfa3af278
Shankar <212604198@ge.com>
2021-06-24 11:45:22 +0530

    ML4 release

--- src/app/dummy.service.ts
+++ src/app/dummy.service.ts
@@ -7,15 +7,10 @@ import { BehaviorSubject, ReplaySubject } from 'rxjs';
 export class DummyService {
 
   behavior$ = new BehaviorSubject(2);
-  replay$ = new ReplaySubject(1);
 
-  private registeredComponents = [];
 
   constructor() {
     (window as any).dummyService = this;
   }
 
-  register(component) {
-    this.registeredComponents.push(component);
-  }
 }
--- src/app/sub.component.ts
+++ src/app/sub.component.ts
@@ -12,68 +12,34 @@ export class SubComponent implements OnDestroy {
   rand = Math.random();
   rand2 = 0;
   subject = new BehaviorSubject(42);
+  subVariable;
 
   arr = [];
 
-  /** Emits when the component is destroyed */
-  private destroy$: Subject<void> = new Subject<void>();
 
   constructor(private dummyService: DummyService) {
     for (let i = 0; i < 100000; ++i) {
       this.arr.push(Math.random());
     }
 
-    // no leak - component is cleaned up
-    // this.subject.subscribe();
 
-    // no leak - component is cleaned up
-    // this.subject.subscribe(() => {const blub = 34;});
-
-    // no leak - component is cleaned up
-    // this.subject.subscribe(() => this.rand2 = 33);
-
-    // small leak - empty anonymous function remains in heap
-    // this.dummyService.behavior$.subscribe();
-    // FIXED
-    // this.dummyService.behavior$.pipe(takeUntil(this.destroy$)).subscribe();
-
-    // small leak - small anonymous function remains in heap
-    // this.dummyService.behavior$.subscribe(() => {const blub = 34;});
-    // FIXED
-    // this.dummyService.behavior$.pipe(takeUntil(this.destroy$)).subscribe(() => {});
-
-    // (!) large leak - anonymous function references the component, thus the component stays in memory
-    // this.dummyService.behavior$.subscribe(() => this.rand2 = 33);
-    // FIXED
-    // this.dummyService.behavior$.pipe(takeUntil(this.destroy$)).subscribe(() => this.rand2 = 33);
-
-    // no leak
-    // this.dummyService.behavior$.pipe(tap(() => this.rand2 = 44), publish()).subscribe(() => this.rand2 = 33);
-
-    // no leak
-    // this.dummyService.behavior$.pipe(tap(() => this.rand2 = 44), publishBehavior(3), takeUntil(this.destroy$)).subscribe(() => this.rand2 = 33);
-
-    // no leak
-    // this.dummyService.behavior$.pipe(tap(() => this.rand2 = 44), publishLast(), takeUntil(this.destroy$)).subscribe(() => this.rand2 = 33);
-
-    // no leak
-    // this.dummyService.behavior$.pipe(tap(() => this.rand2 = 44), publishReplay(), takeUntil(this.destroy$)).subscribe(() => this.rand2 = 33);
-
-    // (!) large leak - the subscription from the shareReplay operator to the source is never unsubscribed, thus the component stays in the heap
-    this.dummyService.behavior$.pipe(tap(() => this.rand2 = 44), shareReplay(1), takeUntil(this.destroy$)).subscribe(() => this.rand2 = 33);
-    // FIXED
-    // this.dummyService.behavior$.pipe(tap(() => this.rand2 = 44), takeUntil(this.destroy$), shareReplay(1), takeUntil(this.destroy$)).subscribe(() => this.rand2 = 33);
-
-    // no leak
-    // this.dummyService.behavior$.pipe(tap(() => this.rand2 = 44), shareReplay({bufferSize: 1, refCount: true})).subscribe(() => this.rand2 = 33);
-
-    // (!) large leak - the component reference in the service is not cleaned up, thus the component stays in memory
-    // this.dummyService.register(this);
+    /*
+    this.dummyService.behavior$.subscribe(value => {
+      //Assign the value to a new variable
+      const foo =value;
+    })
+    */
+    
+    this.dummyService.behavior$.subscribe(value => {
+      //Assing the value to the component variable
+      this.rand =value;
+    })
+    
+    
   }
 
   ngOnDestroy(): void {
-      this.destroy$.next();
-      this.destroy$.complete();
+    
   }
 
 }
