3ffeb56b9c53e80d154f8194e361c0361d695a52
Shankar <212604198@ge.com>
2021-06-24 13:06:43 +0530

    test

--- src/app/sub.component.ts
+++ src/app/sub.component.ts
@@ -23,18 +23,19 @@ export class SubComponent implements OnDestroy {
     }
 
 
-    
+    /*
     this.dummyService.behavior$.subscribe(value => {
       //Assign the value to a new variable
       const foo =value;
     })
+    */
+
     
-    /*
     this.dummyService.behavior$.subscribe(value => {
       //Assing the value to the component variable
       this.rand =value;
     })
-    */
+    
     
     
   }
